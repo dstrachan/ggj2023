765caa6e3dcc35c948c28346de587663ee5ba37c
David Strachan <davidstrachan92@gmail.com>
2023-02-05 13:57:40 +0000

    Ensure required bottles are available

--- Assets/Scripts/FactoryTrigger.cs
+++ Assets/Scripts/FactoryTrigger.cs
@@ -15,6 +15,7 @@ public class FactoryTrigger : MonoBehaviour
     private Vector3[][] _spawnerPositions;
     private GameObject[] _customers;
     private int[] _indices;
+    private Stack<BottleColor> _requiredColors;
 
     private static readonly int EnumCount = Enum.GetValues(typeof(BottleColor)).Length;
 
@@ -24,6 +25,7 @@ public class FactoryTrigger : MonoBehaviour
 
         PopulateTilemapGroupTransforms(customersTilemap);
 
+        _requiredColors = new Stack<BottleColor>();
         _customers = new GameObject[_spawnerPositions.Length];
         _indices = new int[_spawnerPositions.Length];
         for (var i = 0; i < _spawnerPositions.Length; i++)
@@ -44,10 +46,12 @@ public class FactoryTrigger : MonoBehaviour
             }
 
             print("LOADING CAR");
+            _requiredColors.Clear();
             for (var i = 0; i < maxBottles; i++)
             {
                 var color = (BottleColor)_random.Next(0, EnumCount);
                 carInventory.AddBottle(color);
+                _requiredColors.Push(color);
             }
 
             RespawnCustomers();
@@ -115,7 +119,12 @@ public class FactoryTrigger : MonoBehaviour
             var spawnerPositions = _spawnerPositions[index];
             var spawnerIndex = _random.Next(spawnerPositions.Length);
             _customers[index] = Instantiate(customerPrefab, spawnerPositions[spawnerIndex], Quaternion.identity);
-            _customers[index].GetComponent<CustomerTrigger>().SetDemand((BottleColor)(i % EnumCount));
+            if (!_requiredColors.TryPop(out var color))
+            {
+                color = (BottleColor)(i % EnumCount);
+            }
+
+            _customers[index].GetComponent<CustomerTrigger>().SetDemand(color);
             i += 1;
         }
     }
