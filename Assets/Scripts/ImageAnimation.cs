fb2332fc35fae5d446bfd63e3478e98edaedcc9b
Michael Gillon <Michael@blocksui.com>
2023-02-05 14:20:07 +0000

    main scene changes

--- Assets/Scripts/ImageAnimation.cs
+++ Assets/Scripts/ImageAnimation.cs
@@ -5,28 +5,38 @@ using UnityEngine.UI;
 public class ImageAnimation : MonoBehaviour {
 
     public Sprite[] sprites;
-    public int spritePerFrame = 6;
+    public float frameTimeSeconds = 6;
     public bool loop = true;
     public bool destroyOnEnd = false;
 
+    private float lastFrame;
+    
     private int index = 0;
     private Image image;
     private int frame = 0;
 
     void Awake() {
         image = GetComponent<Image> ();
+        lastFrame = Time.time;
     }
 
-    void Update () {
-        if (!loop && index == sprites.Length) return;
-        frame ++;
-        if (frame < spritePerFrame) return;
-        image.sprite = sprites [index];
-        frame = 0;
-        index ++;
-        if (index >= sprites.Length) {
-            if (loop) index = 0;
-            if (destroyOnEnd) Destroy (gameObject);
+    void Update() 
+    {
+        if (!loop && index == sprites.Length)
+        {
+            return;
+        }
+
+        if (lastFrame + frameTimeSeconds < Time.time)
+        {
+            image.sprite = sprites[index];
+            lastFrame = Time.time;
+            index++;
+            if (index >= sprites.Length)
+            {
+                if (loop) index = 0;
+                if (destroyOnEnd) Destroy(gameObject);
+            }
         }
     }
 }
